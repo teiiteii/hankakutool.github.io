eb0316a85ed55f820ef8eaa81c0a2a0afb3e7048
teiiteii <test>
2020-05-06 18:37:05 +0900

    .;

--- data/memo.js
+++ data/memo.js
@@ -1,5 +1,8 @@
 const memo =[
 {tesf:"リンク", tess:"横強", fighter_id:1030, skill_genre:10501, memo:"0距離でガードされると持続当てになる"} 
+{tesf:"リンク", tess:"横強", fighter_id:1030, skill_genre:10601, memo:"22Fから2段目以降可能"} 
+
+
 ,{tesf:"リンク", tess:"横B", fighter_id:1030, skill_genre:10701, memo:"5m以内が本当て<br>キャッチは9F。地上空振り59Fで、空中空振り45F<br>戻りのガード硬直は7Fで、5連続当てても不変"} 
 ,{tesf:"リンク", tess:"下B", fighter_id:1030, skill_genre:10702, memo:"発生1F保証<br>取り出し39F"} 
 ,{tesf:"リンク", tess:"アイテム", fighter_id:1030, skill_genre:10101, memo:"ガード硬直6F<br>弾速が遅いとガード硬直が発生しないときがある？"} 
